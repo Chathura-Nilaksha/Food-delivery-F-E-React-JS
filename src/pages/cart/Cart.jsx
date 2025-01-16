676d49121d6de560a182e763a7015d77673a8f2c
Chathura-Nilaksha <cnilakshami@gmail.com>
2025-01-16 18:48:11 +0530

    Developed cart component

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -39,9 +39,11 @@ const Cart = () => {
         })}
       </div>
 
+
       <div className="cart-bottom">
+
         <div className="cart-total">
-          <h2>Cart Total</h2>
+          <h2>Cart Totals</h2>
 
           <div>
             <div className="cart-total-details">
